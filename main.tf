e64ef67e7fcd4664561f26d9e2c7f5e03caee6cd
Arnaud Lheureux <arnaudlh@users.noreply.github.com>
2020-07-06 10:49:16 +0800

    Update main.tf

--- main.tf
+++ main.tf
@@ -2,4 +2,13 @@ provider azurerm {
   features {}
 }
 
+terraform {
+  required_providers {
+    azurecaf = {
+      source = "aztfmod/azurecaf"
+    }
+  }
+}
+
+
 data "azurerm_subscription" "primary" {}
